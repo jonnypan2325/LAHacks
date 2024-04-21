d6eb38d6d2cf287f058acf7ca130ddc473e76dd1
eriyoungg <erikayoung314@gmail.com>
2024-04-21 01:35:16 -0700

    my last straw

--- chrome_extension/background.js
+++ chrome_extension/background.js
@@ -1,14 +1,14 @@
 // Define function to continuously retrieve data
 function retrieveData() {
-  chrome.storage.local.get('myData', function(result) {
-      var data = result.myData || {};
-      console.log('Retrieved data:', data);
-  });
+    chrome.storage.local.get('myData', function(result) {
+        var data = result.myData || {};
+        console.log('Retrieved data:', data);
+    });
 }
 
 // Run function continuously
 setInterval(function() {
-  retrieveData();
+    retrieveData();
 }, 1000);
 
 // chrome.runtime.onInstalled.addListener(() => {
